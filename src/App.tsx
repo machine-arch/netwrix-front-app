0cb0334b56e06de469089bd266dc3d34a1f4efd8
unknown <merabkhatiashvili@gmail.com>
2023-02-06 20:32:50 +0400

    upload project

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Fragment, useContext, useEffect, useState } from "react";
+import "./App.scss";
+import Header from "./components/header/Header.component";
+import ResultCard from "./components/result-card/result-card.component";
+import Loader from "./components/loader/Loader.component";
+import { partnersContext } from "./context/partners.context";
 
 function App() {
+  const [showUpTpTopButton, setShowUpTpTopButton] = useState(false);
+  const showHideBackToTop = (e: any) => {
+    e.preventDefault();
+    if (window.scrollY >= 300) {
+      setShowUpTpTopButton(true);
+    } else {
+      setShowUpTpTopButton(false);
+    }
+  };
+  useEffect(() => {
+    window.addEventListener("scroll", showHideBackToTop);
+    return () => window.removeEventListener("scroll", showHideBackToTop);
+  }, [showUpTpTopButton]);
+
+  const backTotop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <div className="netwrix_App">
+        <Loader isLoading={true} />
+        <Header />
+        <ResultCard />
+        {showUpTpTopButton && (
+          <span id="back-to-top-button" onClick={backTotop}>
+            <img src="assets/ico/uparrow.svg" alt="up_arrow"></img>
+          </span>
+        )}
+      </div>
+    </Fragment>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.scss";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { PartnersProvider } from "./context/partners.context";
+import { DropDownsProvider } from "./context/dropdowns.context";
+import { LoaderProvider } from "./context/loader.context";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <PartnersProvider>
+      <DropDownsProvider>
+        <LoaderProvider>
+          <App />
+        </LoaderProvider>
+      </DropDownsProvider>
+    </PartnersProvider>
   </React.StrictMode>
 );
 
